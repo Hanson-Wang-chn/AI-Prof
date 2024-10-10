07a451554191564611e1112fe745cfa382fae740
cubic-cat <15961850558@163.com>
2024-10-10 16:19:05 +0800

    Add files via upload
    the correct (up to now) code

---
+++ backend/OlogyProfBackendDemo/src/main/java/com/example/ologyprofbackenddemo/repository/IHistoryRepo.java
@@ -0,0 +1,7 @@
+package com.example.ologyprofbackenddemo.repository;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.ologyprofbackenddemo.model.DO.HistoryDO;
+
+public interface IHistoryRepo extends IService<HistoryDO> {
+}
