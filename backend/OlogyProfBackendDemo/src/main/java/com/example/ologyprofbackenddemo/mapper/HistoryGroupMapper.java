5fc0fd299b6d4ca68d14808db0924503efd89880
cubic-cat <15961850558@163.com>
2024-10-12 14:22:23 +0800

    Delete backend/OlogyProfBackendDemo directory

--- backend/OlogyProfBackendDemo/src/main/java/com/example/ologyprofbackenddemo/mapper/HistoryGroupMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.ologyprofbackenddemo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.ologyprofbackenddemo.model.DO.HistoryGroupDO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface HistoryGroupMapper extends BaseMapper<HistoryGroupDO>{
-}
--- backend/OlogyProfBackendDemo/src/main/java/com/example/ologyprofbackenddemo/repository/IHistoryGroupRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.ologyprofbackenddemo.repository;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.ologyprofbackenddemo.model.DO.HistoryGroupDO;
-
-public interface IHistoryGroupRepo extends IService<HistoryGroupDO> {
-}
